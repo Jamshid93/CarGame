5d2d8cf2bfbb26af4d24fdc2bd63d9230c597c7c
Jamshid93 <hamzayevjf@gmail.com>
2019-07-19 19:39:45 +0500

    there are new code about car Game

--- carGame.py
+++ carGame.py
@@ -1,15 +1,24 @@
 command=""
+started = False
 while command !="quit":
     command = input("> ").lower()
     if command=="start":
-        print("Car started...")
+        if started:
+            print("Car is alredy started!")
+        else:
+            started = True
+            print("Car started...")
     elif command== "stop":
-        print("Car stopped.")
+        if not started:
+            print("Car is alredy stopped!")
+        else:
+            started = False
+            print("Car stopped.")
     elif command =="help":
         print("""
-        start - to strat the car
-        stop - to stop the car
-        quit - to quit
+start - to strat the car
+stop - to stop the car
+quit - to quit
         """)
     else:
         print("Sorry, I don't understand that!")
